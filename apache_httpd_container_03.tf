7916e7eeeb7cdfec975ae7c6dc1d14a119efffe6
Todorov <ltodorov3@dxc.com>
2020-03-31 17:38:04 +0300

    version 03

--- apache_httpd_container_03.tf
+++ apache_httpd_container_03.tf
@@ -19,14 +19,14 @@ resource "docker_container" "httpd" {
   image = "${docker_image.blubird_image.name}"
   volumes {
     volume_name = "${docker_volume.blubird_volume.name}"
-	container_path = "/var/www/html"
+    container_path = "/var/www/html"
   }
   networks_advanced {
     name = "${docker_network.blubird_network.name}"
   }
   ports {
     internal = "80"
-	external = "8080"
+    external = "8080"
   }
   restart = "always"
 }
