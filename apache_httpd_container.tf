3149104871228868b3ae91ebbc214ff6f1871a20
Todorov <ltodorov3@dxc.com>
2020-04-01 14:35:35 +0300

    newest version

--- apache_httpd_container.tf
+++ apache_httpd_container.tf
@@ -1,21 +1,28 @@
+variable "docker_container_name" {}
+variable "docker_volume_name" {}
+variable "docker_network_name" {}
+variable "docker_image_name" {
+  default = "httpd:latest"
+}
+
 provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
 resource "docker_image" "blubird_image" {
-  name = "httpd:latest"
+  name = var.docker_image_name
 }
 
 resource "docker_network" "blubird_network" {
-  name = "blubird_network"
+  name = var.docker_network_name
 }
 
 resource "docker_volume" "blubird_volume" {
-  name = "blubird_volume"
+  name = var.docker_volume_name
 }
 
-resource "docker_container" "httpd" {
-  name  = "blubird"
+resource "docker_container" "blubird_container" {
+  name  = var.docker_container_name
   image = docker_image.blubird_image.name
   volumes {
     volume_name    = docker_volume.blubird_volume.name
