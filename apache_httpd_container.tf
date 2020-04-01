d3da35ae06bb9a050645a2496a1b85f0789222a1
Todorov <ltodorov3@dxc.com>
2020-04-01 13:25:07 +0300

    upload

--- apache_httpd_container.tf
+++ apache_httpd_container.tf
@@ -2,11 +2,31 @@ provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
+resource "docker_image" "blubird_image" {
+  name = "httpd:latest"
+}
+
+resource "docker_network" "blubird_network" {
+  name = "blubird_network"
+}
+
+resource "docker_volume" "blubird_volume" {
+  name = "blubird_volume"
+}
+
 resource "docker_container" "httpd" {
   name  = "blubird"
-  image = "httpd:latest"
+  image = docker_image.blubird_image.name
   volumes {
-    volume_name = "blubird_test"
-    container_path = "/www/var/html"
+    volume_name    = docker_volume.blubird_volume.name
+    container_path = "/var/www/html"
   }
+  networks_advanced {
+    name = docker_network.blubird_network.name
+  }
+  ports {
+    internal = "80"
+    external = "8080"
+  }
+  restart = "always"
 }
